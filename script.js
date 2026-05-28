5f9bad7a27e58f75b1de81594abe440fc29b6740
Abhimanyu vaid <avaid2@academic.rrc.ca>
2026-05-28 06:08:57 -0500

    feat: currentstate and render question added.

---
+++ script.js
@@ -0,0 +1,16 @@
+const question = document.getElementById("question");
+const answers = document.getElementById("answers");
+const nextBtn = document.getElementById("next-btn");
+
+
+
+// to trach user in the story
+let currentState = "start";
+
+
+function renderQuestion() {
+
+}
+
+
+renderQuestion();
